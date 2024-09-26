e1eb7f9b7197b8c8451b38f4d4f89612ead60983
vacationl33t <m1809131@edu.misis.ru>
2024-09-26 23:15:56 +0300

    v1

--- internal/bitcounter/impl.go
+++ internal/bitcounter/impl.go
@@ -5,8 +5,44 @@ type Input struct {
 }
 
 type Result struct {
+	ByteStatistic []ByteStat `json:"byteStatistic"`
+}
+
+type ByteStat struct {
+	RaisedBitQuantity int               `json:"raisedBitQuantity"`
+	ByteCount         int               `json:"byteCount"`
+	Values            map[int][]int     `json:"values"`
+}
+
+func countSetBits(n byte) int {
+	count := 0
+	for n > 0 {
+		count += int(n & 1)
+		n >>= 1
+	}
+	return count
 }
 
 func Process(in *Input) *Result {
-	return &Result{}
+	result := &Result{
+		ByteStatistic: make([]ByteStat, 8), // Максимум 8 бит
+	}
+
+	// Инициализация структуры для хранения значений
+	for i := 0; i < len(result.ByteStatistic); i++ {
+		result.ByteStatistic[i].RaisedBitQuantity = i
+		result.ByteStatistic[i].Values = make(map[int][]int)
+	}
+
+	// Подсчет бит и заполнение статистики
+	for offset, b := range in.Data {
+		raisedBitQuantity := countSetBits(b)
+		result.ByteStatistic[raisedBitQuantity].ByteCount++
+		if _, exists := result.ByteStatistic[raisedBitQuantity].Values[int(b)]; !exists {
+			result.ByteStatistic[raisedBitQuantity].Values[int(b)] = []int{}
+		}
+		result.ByteStatistic[raisedBitQuantity].Values[int(b)] = append(result.ByteStatistic[raisedBitQuantity].Values[int(b)], offset)
+	}
+
+	return result
 }
--- internal/httphandlers/page_handler.go
+++ internal/httphandlers/page_handler.go
@@ -5,43 +5,89 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
 const (
-	_parseMaxMemory = 100 * 1024 * 1024
+	_parseMaxMemory = 100 * 1024 * 1024 // 100 MB
 )
+
+type FileInfo struct {
+	Filename string `json:"filename"`
+	Size     int64  `json:"size"`
+}
+
+type Statistics struct {
+	RequestCount int       `json:"requestCount"`
+	Files        []FileInfo `json:"files"`
+}
 
 type pageHandler struct {
 	formFilePath string
+	mu           sync.Mutex // Для синхронизации доступа к статистике
+	stats        Statistics
 }
 
-func (ph pageHandler) Web(c *gin.Context) {
+func (ph *pageHandler) Web(c *gin.Context) {
 	t, err := template.ParseFiles(ph.formFilePath)
 	if err != nil {
-		log.WithError(err).Fatal("Can`t parse files")
+		log.WithError(err).Error("Can't parse files")
+		c.String(http.StatusInternalServerError, "Internal Server Error")
+		return
 	}
 
 	if err := t.Execute(c.Writer, nil); err != nil {
-		log.WithError(err).Fatal("Can`t execute template")
+		log.WithError(err).Error("Can't execute template")
+		c.String(http.StatusInternalServerError, "Internal Server Error")
+		return
 	}
 }
 
-func (ph pageHandler) Process(c *gin.Context) {
-	c.Request.ParseMultipartForm(_parseMaxMemory)
+func (ph *pageHandler) Process(c *gin.Context) {
+	if err := c.Request.ParseMultipartForm(_parseMaxMemory); err != nil {
+		log.WithError(err).Error("Error parsing multipart form")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
 
 	fileHeader := c.Request.MultipartForm.File["binary_input_data"][0]
 
-	file, _ := fileHeader.Open()
+	file, err := fileHeader.Open()
+	if err != nil {
+		log.WithError(err).Error("Error opening file")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not open file"})
+		return
+	}
 	defer file.Close()
 
-	data, _ := io.ReadAll(file)
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.WithError(err).Error("Error reading file")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not read file"})
+		return
+	}
 
 	log.Debugf("Received data: %+v", data)
 
-	result := bitcounter.Process(&bitcounter.Input{Data: data})
+	// Обновляем статистику
+	ph.mu.Lock()
+	ph.stats.RequestCount++
+	ph.stats.Files = append(ph.stats.Files, FileInfo{
+		Filename: fileHeader.Filename,
+		Size:     fileHeader.Size,
+	})
+	ph.mu.Unlock()
 
+	result := bitcounter.Process(&bitcounter.Input{Data: data})
 	c.JSON(http.StatusOK, result)
+}
+
+func (ph *pageHandler) Stat(c *gin.Context) {
+	ph.mu.Lock()
+	defer ph.mu.Unlock()
+
+	c.JSON(http.StatusOK, ph.stats)
 }
